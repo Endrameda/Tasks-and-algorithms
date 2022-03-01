f24b1a049747efae3153667a7b0b4636db869260
Sardor <Endrameda98@gmail.com>
2022-03-01 10:55:24 +0500

    add git ignore and restructured folters

--- algorithms/bubbleSort/index.js
+++ algorithms/bubbleSort/index.js
@@ -30,6 +30,7 @@ function bubbleSort(array) {
         for (let i = 0; i < array.length - 1 + n; i++) {
             if (array[i] > array[i + 1]) {
                 const buffer = array[i]
+
                 array[i] = array[i + 1]
                 array[i + 1] = buffer
             }
